7dff11547728efd710410c2af5cb10a36f444c4f
daniel2009 <dl2648@nyu.edu>
2015-08-07 12:53:57 -0400

    Blog Upload

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   
-  validates :title, presences: true, length: { minimum: 5 }
-  validates :body, presences: true
+  validates :title, presence: true, length: { minimum: 5 }
+  validates :body, presence: true
 end
